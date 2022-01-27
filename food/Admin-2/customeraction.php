4d9233b7b17dc65b4d68f17e444d910a51ef5763
maulikkapupara <91789733+maulikkapupara@users.noreply.github.com>
2022-01-27 12:11:18 -0800

    new admin pannel

--- food/Admin-2/customeraction.php
+++
@@ -1,7 +0,0 @@
-<?php 
-include ("include/config.php");
-$id = $_GET['id'];
-// echo $id;
-mysqli_query($conn,"DELETE FROM  customer WHERE id='$id'");
-header("location:customer.php");
-?>
--- food/Admin-2/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-	session_unset();
-	$_SESSION['logout'] = "You are successfully logout...";
-	header('Location: index.php');
-
-?>
